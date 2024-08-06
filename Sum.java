90dbae7ad7ef85358d94519667587e4a7a9ec797
JagtapSiddhant11 <siddhantjagtap44@gmail.com>
2024-08-06 15:44:25 +0530

    commit made

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+
+    public static void main(String[] args) {
+        int a , b , c;
+        a = 5; 
+        b = 9;
+        c = a + b;
+        System.out.println(c);
+    }
+}
